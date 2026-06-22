6c762666f18f4ea220f6dd49221ce58577489b38
alnaimat01 <mohmmed.n249@gmail.com>
2026-06-22 20:50:56 +0300

    Use new proxy server

--- shadcn-ui/src/components/DeliverooScraper.tsx
+++ shadcn-ui/src/components/DeliverooScraper.tsx
@@ -25,7 +25,7 @@ const handleGetDeliverooSource = async () => {
   setIsFetchingSource(true);
 
   try {
-    const response = await fetch('https://menu-scraper1.onrender.com/fetch', {
+    const response = await fetch('https://menu-scraper-platform1.onrender.com/fetch', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- shadcn-ui/src/components/TalabatScraper.tsx
+++ shadcn-ui/src/components/TalabatScraper.tsx
@@ -155,7 +155,7 @@ export function TalabatScraper() {
         setIsFetchingSource(true);
       
         try {
-          const response = await fetch('https://menu-scraper1.onrender.com/fetch', {
+          const response = await fetch('https://menu-scraper-platform1.onrender.com/fetch', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
